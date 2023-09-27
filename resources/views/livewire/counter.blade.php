076e055c2d3d14b67b34fc87888150d2431374b8
Fernando Rocha <beneditofernandor@gmail.com>
2023-09-26 22:02:47 -0300

    Counter

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,7 @@
 <div>
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <button wire:click="increment">+</button>
+    <span>{{ $count }}</span>
+    <button wire:click="decrement">-</button>
+
 </div>

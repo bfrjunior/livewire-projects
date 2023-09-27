d52317b45fc6956059959c7ce501f6638e0d77a1
Fernando Rocha <beneditofernandor@gmail.com>
2023-09-27 00:25:13 -0300

    Image Upload

--- app/Http/Livewire/ProductSearch.php
+++ app/Http/Livewire/ProductSearch.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ProductSearch extends Component
 {
+    public string $search = '';
+
     public function render()
     {
-        return view('livewire.product-search');
+        $query = Product::query();
+        if ($this->search) {
+            $query->where('title', 'like', "%{$this->search}%")
+                ->orWhere('description', 'like', "%{$this->search}%");
+        }
+
+        return view('livewire.product-search', [
+            'products' => $query->paginate(20)
+        ]);
+    }
+
+    public function updated($property)
+    {
+        if ($property === 'search') {
+            $this->resetPage();
+        }
     }
 }

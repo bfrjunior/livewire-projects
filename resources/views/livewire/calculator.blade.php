4c5d2642b2b25dd66d7a9ea141fc6455fe31e267
Fernando Rocha <beneditofernandor@gmail.com>
2023-09-26 22:34:47 -0300

    calculator

---
+++ resources/views/livewire/calculator.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <div class="flex flex-col items-center">
+        <div class="flex p-16 mx-auto justify-center items-center gap-4">
+            <input type="number" wire:model="number1" placeholder="Number 1">
+        </div>
+    </div>
+</div>
